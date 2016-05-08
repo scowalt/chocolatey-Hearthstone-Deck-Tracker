ce670358ad40bab0dc12583377db530573aa2357
Scott Walters <me@scowalt.com>
2016-05-08 15:18:51 -0700

    Initial package

---
+++ Hearthstone-Deck-Tracker/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'Hearthstone Deck Tracker'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/HearthSim/Hearthstone-Deck-Tracker/releases/download/v0.14.7/Hearthstone.Deck.Tracker-v0.14.7.zip'
+
+$desktop    = [Environment]::GetFolderPath("Desktop")
+$shortcut   = Join-Path $desktop "$packageName.lnk"
+$unzipDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$target     = Join-Path $unzipDir "$packageName\$packageName.exe"
+
+Install-ChocolateyZipPackage -PackageName $packageName -Url $url -UnzipLocation $unzipDir
+Install-ChocolateyShortcut -ShortcutFilePath $shortcut -TargetPath $target
---
+++ Hearthstone-Deck-Tracker/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'Hearthstone Deck Tracker'
+$zipName     = 'Hearthstone.Deck.Tracker-v0.14.7.zip'
+
+$desktop     = [Environment]::GetFolderPath("Desktop")
+$shortcut    = Join-Path $desktop "$packageName.lnk"
+
+Uninstall-ChocolateyZipPackage $packageName $zipName
+Remove-Item $shortcut
